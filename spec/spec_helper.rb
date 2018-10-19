e6ff07b074f9fcf340508e0c4ea8bf97b924b8cd
Robin Neumann <robin.neumann@posteo.de>
2018-10-19 16:59:04 +0200

    Drop spec coverage testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,22 +1,2 @@
-begin
-  require 'simplecov'
-  require 'coveralls'
-
-  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-    SimpleCov::Formatter::HTMLFormatter,
-    Coveralls::SimpleCov::Formatter
-  ]
-
-  SimpleCov.start do
-    add_filter '/spec/'
-  end
-rescue LoadError
-end
-
 require File.expand_path(File.dirname(__FILE__) + '/../lib/saxophone')
 Saxophone.handler = ENV['HANDLER'].to_sym if ENV['HANDLER']
-
-RSpec.configure do |config|
-  config.run_all_when_everything_filtered = true
-  config.filter_run :focus
-end
